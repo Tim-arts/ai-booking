26dd94e8b9c0d93c2ed75d7ae261dfe684a0c0ed
Tim.art <timothee.dhenain@gmail.com>
2025-06-29 10:11:55 -0400

    Updated package-lock.json

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { providePrimeNG } from 'primeng/config';
 import { RouterOutlet } from '@angular/router';
 
 import { routes } from './app/app.routes';
@@ -27,6 +28,11 @@ bootstrapApplication(App, {
   providers: [
     provideRouter(routes),
     provideHttpClient(withFetch()),
-    provideAnimations()
+    provideAnimations(),
+    providePrimeNG({
+      theme: {
+        preset: 'lara'
+      }
+    })
   ]
 }).catch(err => console.error(err));
